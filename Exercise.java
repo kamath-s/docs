95a5e9187bee1bb2e93fd77b52248d52f66ce816
kamath-s <54655263+kamath-s@users.noreply.github.com>
2020-04-03 12:48:17 +0530

    Add files via upload

---
+++ Exercise.java
@@ -0,0 +1,83 @@
+package git;
+
+public class Exercise {
+
+	
+		// TODO Auto-generated method stub
+		/*
+		
+		27-March
+		------------
+		1. Use other browser - Firefox & Microsoft EDGE (or Internet explorer)
+		2. rediffmail - try to add data
+		3. Select class... 
+		
+		-----
+		30-March
+		-----
+		
+		4. 
+		    a. Find the total of all the elements in the page
+		    b. Find the unique tagnames in the page
+		    c. Give the tagname and count in the page
+		    
+		    1. //* - xpath - Everything in page
+		    2. elements - findelements 
+		    3. create a set of tagnames 
+		    4. Loop through all elements and add unique tagnames to the set
+		    5. Create a map <String, integer>
+		    6. Loop through the set and use findelements (size) - to get a count of each tagname
+		    7. Add to the map object all the tagnames and count  
+		    
+		    Total no. of elements : XXX
+		    Unique tag: a, div, input ........
+		    Count of tag: a,2; div,25; input,33;.........
+		    
+		-----
+		31-Mar
+		-----
+		
+		First - Working on tech stuff - understanding how function works / set / get / ...
+		Second - Rediff register - fill the form completely
+					  - automationpractice.com -- register - login - search - item
+					  - Generic - function 
+					  - Smaller chunks - 
+					  - Signup - username - "cts-s003"
+		-----
+		2-April-
+		----
+		TestNG 
+		1.  Try out all the other Annotations:
+		 https://www.javatpoint.com/testng-annotations
+		 @Beforeclass & After/ @BeforeTest & After / @BeforeSuite & After
+		 
+		2. Convert few of the other java projects / programs in to testNG
+		
+		----
+		3-April-2020
+		----
+		1. Parallel tests - Try with selenium scripts - 
+		2. Parameters - Dataprovider - Use it in the below scenario
+		 - Login to automationpractice.com using Dataprovider function in TestNG
+		3. Searchprovider returning different parameters based on the calling function
+		 - If login function calls - send - username and password
+		 - If search calls, send the search string
+		 
+		 Practice websites for end to end flow:
+		 http://automationpractice.com/index.php
+		 http://newtours.demoaut.com/ - (Mercury tours)
+		 
+		 Practice site with regards to controls etc. -
+		 http://the-internet.herokuapp.com/
+		 
+		 https://www.techbeamers.com/websites-to-practice-selenium-webdriver-online/
+		 
+		 4. Check assert for a webpage (Rediff)
+		 5. Check pageloadtimeout for timesofindia.com 
+		 
+		 *
+		 */
+		
+
+
+}
